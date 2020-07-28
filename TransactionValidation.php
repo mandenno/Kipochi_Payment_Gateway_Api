9002d0fdf10a3d270fea633a413ab61baf8cb193
Dennis Onkangi <denonkangi@gmail.com>
2020-07-28 09:31:24 +0300

    Create TransactionValidation.php

---
+++ TransactionValidation.php
@@ -0,0 +1,17 @@
+<?php
+        include_once('KipochiPaymentGateway.php');  
+       
+        $apiKey="APIKEY";//Merchant's API KEY
+        
+        $username="USERNAME"; //Merchant's API USERNAME'
+		
+		    $transId="TRANSACTION_CODE";//transaction ID to query
+      
+        $gway = new KPG();
+       
+        $response = $gway->validatePayment($apiKey, $username, $transId);
+       
+        echo $response;
+       
+
+?>
